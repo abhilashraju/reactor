86c216a0cff4024d1951d61317939efb42eb9b9c
Abhilash Raju <abhilash.kollam@gmail.com>
2023-12-16 12:46:13 -0600

    removing redfish-event plugin

--- examples/plugins/redfish_events/bmcwebhook.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-#include <string>
-class BmcWebHooks
-{
-  public:
-    virtual std::string registerRoutes() = 0; // Pure virtual function
-    virtual ~BmcWebHooks() = default;  // Virtual destructor
-};
